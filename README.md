f3c10de08e5492fc5f1f3d89874cdaafd9152cf8
hezhaoshun <hezhaoshun@cmcm.com>
2022-03-24 16:59:19 +0800

    feat: select subtitles

---
+++ README.md
@@ -0,0 +1,21 @@
+#### youtube-select-subtitles
+
+chrome插件，在YouTube播放视频时，自动选中视频的第一条字幕
+
+#### 背景
+YouTube 在web端不会默认显示`自动生成的字幕`，即使在YouTube设置中的 播放和效果 > 字幕，
+勾选了 `总是显示字幕` 和 `包含自动生成的字幕（若有）`，也是无效。所以开发了这个chrome插件，
+可以自动选择第一条字幕并显示。
+
+#### 使用方法
+
+##### 拉取 youtube-select-subtitles
+```
+git clone git@github.com:josonho/youtube-select-subtitles.git
+```
+
+##### 在谷歌浏览器安装拓展
+在谷歌浏览依次打开 更多工具 > 拓展程序
+<br/>
+然后就可以看到 `加载已解压的拓展程序` 按钮，点击 `加载已解压的拓展程序`，并选择刚刚拉取至本地的 `youtube-select-subtitles`，
+整个文件夹添加即可
--- content.js
+++ content.js
@@ -1,27 +1,104 @@
-function creatTimer() {
-  const timer = setTimeout(() => {
-    try {
-      selectFirstSubtitles()
-      console.log('选中字符成功')
-      clearTimeout(timer)
-    } catch (error) {
-      console.error(error)
-      console.log('选中字幕失败, 正在重新尝试...')
-      creatTimer()
-    }
-  }, 500)
+let cacheUrl = window.location.href
+
+creatListenUrlTimer()
+handleUrlChange()
+
+function creatListenUrlTimer() {
+	setInterval(() => {
+		console.log('等待网址发生变化...')
+		if (window.location.href !== cacheUrl) {
+			cacheUrl = window.location.href
+			handleUrlChange()
+		}
+	}, 500)
 }
-creatTimer()
+
+function handleUrlChange() {
+	if (!window.location.pathname.includes('watch')) {
+		return
+	}
+
+	selectFirstSubtitles()
+}
 
 /**
  * 选中第一项字符
  */
-function selectFirstSubtitles () {
-  document.querySelector('.ytp-settings-button').click()
-  const settingsMenuEl = document.querySelector('.ytp-settings-menu')
-  settingsMenuEl.querySelectorAll('.ytp-menuitem')[2].click()
-  setTimeout(() => {
+async function selectFirstSubtitles() {
+	await clickSettingsButton()
+  // 点击设置按钮后，会延迟出现菜单栏
+  await wait(300)
+	clickMenuSubtitles().then(async () => {
+    // 点击字幕设置项后，会延迟出现字幕列表，这里的延迟较长
+    await wait(500)
+    const settingsMenuEl = document.querySelector('.ytp-settings-menu')
     settingsMenuEl.querySelectorAll('.ytp-menuitem')[1].click()
-    document.querySelector('.html5-main-video').click()
-  }, 500);
+    console.log('选中字幕成功')
+    toast('选中字幕成功')
+  }).catch((error) => {
+    toast(error)
+  }).finally(() => {
+  	// 关闭设置弹窗
+    document.querySelector('.ytp-settings-button').click()
+  })
+}
+
+
+/**
+ * 点击设置按钮
+ */
+function clickSettingsButton() {
+	return new Promise((resolve) => {
+		const timer = setInterval(() => {
+			console.log('等待设置按钮加载...')
+
+			const settingsButton = document.querySelector('.ytp-settings-button')
+
+			if (settingsButton) {
+				settingsButton.click()
+				clearInterval(timer)
+				resolve(true)
+			}
+		}, 500)
+	})
+}
+
+/**
+ * 点击菜单的字幕设置
+ */
+function clickMenuSubtitles() {
+  return new Promise((resolve, reject) => {
+    let isSuccess = false
+    const settingsMenuEl = document.querySelector('.ytp-settings-menu')
+    settingsMenuEl.querySelectorAll('.ytp-menuitem').forEach((item) => {
+      if (item?.querySelector('.ytp-menuitem-label span')?.innerHTML === '字幕') {
+        item.click()
+        isSuccess = true
+        resolve(true)
+      }
+    })
+    if (!isSuccess) {
+      reject('没有字幕设置项')
+    }
+  })
+}
+
+function wait(delay) {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(delay)
+		}, delay)
+	})
+}
+
+function toast(msg, duration) {
+	duration = isNaN(duration) ? 3000 : duration
+	const messageEl = document.createElement('div')
+	messageEl.innerHTML = msg
+	messageEl.style.cssText =
+		'font-size: .32rem;color: rgb(255, 255, 255);background-color: rgba(0, 0, 0, 0.6);padding: 10px 15px;margin: 0 0 0 -60px;border-radius: 4px;position: fixed;top: 50%;left: 50%;width: 130px;text-align: center;z-index: 99999999'
+	document.body.appendChild(messageEl)
+  setTimeout(() => {
+    document.body.removeChild(messageEl)
+  }, duration)
 }
